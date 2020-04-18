ae7764afcbb649aac2e657682194d123283e03db
Maksym Stepanenko <maksym.stepanenko@opower.com>
2020-04-18 14:22:43 +0300

    Exercise 35 Long Live Boole fully completed with test cases.

--- src/main/java/com/max/erickson/dynamic/LongLiveBoole.java
+++ src/main/java/com/max/erickson/dynamic/LongLiveBoole.java
@@ -1,6 +1,10 @@
 package com.max.erickson.dynamic;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -42,19 +46,34 @@ public final class LongLiveBoole {
     private static final char[] OPERATORS = {MUL, ADD, XOR};
 
 
-    public static boolean canBeTrue(String exp) {
+    public static Optional<String> findTrueExpression(String exp) {
         checkArgument(exp != null, "null 'exp' passed");
-        return canBeTrue(exp.toCharArray());
+        return findTrueExpressionImpl(exp);
     }
 
-    public static boolean canBeTrue(char[] exp) {
-        checkArgument(exp != null, "null 'exp' passed");
+    public static boolean canBeTrue(String expStr) {
+        return findTrueExpressionImpl(expStr).isPresent();
+    }
+
+    /**
+     * Dynamic programming solution.
+     * N = exp.length
+     * time: O(N^3), we need to spend N time for each cell of N^2 table.
+     * space: O(N^2)
+     */
+    private static Optional<String> findTrueExpressionImpl(String expStr) {
+
+        checkArgument(expStr != null, "null 'exp' string passed");
+
+        final char[] exp = expStr.toCharArray();
         checkExpressionValid(exp);
 
         final int size = exp.length;
 
-        boolean[][] isTrue = new boolean[size][size];
-        boolean[][] isFalse = new boolean[size][size];
+        final boolean[][] isTrue = new boolean[size][size];
+        final boolean[][] isFalse = new boolean[size][size];
+
+        final Map<State, String> solution = new HashMap<>();
 
         // fill both tables simultaneously from bottom row and up
         for (int row = size - 1; row >= 0; row -= 2) {
@@ -63,61 +82,126 @@ public final class LongLiveBoole {
                 if (row == col) {
                     isTrue[row][col] = (BOOLEAN_TRUE == exp[row]);
                     isFalse[row][col] = (BOOLEAN_FALSE == exp[row]);
+                    solution.put(new State(row, col, BOOLEAN_TRUE == exp[row]), String.valueOf(exp[row]));
                 }
                 else {
-
                     for (int k = row + 1; k < col; k += 2) {
 
                         assert isValidOperator(exp[k]) : "Not a valid operator";
 
                         switch (exp[k]) {
-                            case MUL -> updateMul(isTrue, isFalse, row, k, col);
-                            case ADD -> updateAdd(isTrue, isFalse, row, k, col);
-                            case XOR -> updateXor(isTrue, isFalse, row, k, col);
+                            case MUL -> updateMul(isTrue, isFalse, solution, row, k, col);
+                            case ADD -> updateAdd(isTrue, isFalse, solution, row, k, col);
+                            case XOR -> updateXor(isTrue, isFalse, solution, row, k, col);
                         }
                     }
                 }
             }
         }
 
-        return isTrue[0][isTrue.length - 1];
+        return Optional.ofNullable(solution.get(new State(0, size - 1, true)));
     }
 
-    private static void updateMul(boolean[][] isTrue, boolean[][] isFalse, int i, int k, int j) {
+    private static final class State {
+        final int i;
+        final int j;
+        final boolean side;
+
+        public State(int i, int j, boolean side) {
+            this.i = i;
+            this.j = j;
+            this.side = side;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (!(obj instanceof State)) {
+                return false;
+            }
+            State that = (State) obj;
+
+            if (i != that.i) {
+                return false;
+            }
+            if (j != that.j) {
+                return false;
+            }
+            return side == that.side;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(i, j, side);
+        }
+
+        @Override
+        public String toString() {
+            return i + ", " + j + ", " + side;
+        }
+    }
+
+    private static void updateMul(boolean[][] isTrue, boolean[][] isFalse, Map<State, String> solution, int i, int k, int j) {
         // update table for TRUE
         if (isTrue[i][k - 1] && isTrue[k + 1][j]) {
             isTrue[i][j] = true;
+            updateSolution(solution, true, MUL, i, k, j, true, true);
         }
 
         // update table for FALSE
         if (isFalse[i][k - 1] || isFalse[k + 1][j]) {
             isFalse[i][j] = true;
+            updateSolution(solution, false, MUL, i, k, j, !isFalse[i][k - 1], !isFalse[k + 1][j]);
         }
     }
 
-    private static void updateAdd(boolean[][] isTrue, boolean[][] isFalse, int i, int k, int j) {
+    private static void updateAdd(boolean[][] isTrue, boolean[][] isFalse, Map<State, String> solution, int i, int k, int j) {
 
         // update table for TRUE
         if (isTrue[i][k - 1] || isTrue[k + 1][j]) {
             isTrue[i][j] = true;
+            updateSolution(solution, true, ADD, i, k, j, isTrue[i][k - 1], isTrue[k + 1][j]);
         }
 
         // update table for FALSE
         if (isFalse[i][k - 1] && isFalse[k + 1][j]) {
             isFalse[i][j] = true;
+            updateSolution(solution, false, ADD, i, k, j, false, false);
         }
     }
 
-    private static void updateXor(boolean[][] isTrue, boolean[][] isFalse, int i, int k, int j) {
+    private static void updateSolution(Map<State, String> solution, boolean curSideValue, char operator, int i, int k, int j,
+                                       boolean leftSideValue, boolean rightSideValue) {
+
+        String leftSide = solution.get(new State(i, k - 1, leftSideValue));
+        String rightSide = solution.get(new State(k + 1, j, rightSideValue));
+
+        solution.put(new State(i, j, curSideValue), String.format("(%s %s %s)", leftSide, operator, rightSide));
+
+    }
+
+    private static void updateXor(boolean[][] isTrue, boolean[][] isFalse, Map<State, String> solution, int i, int k, int j) {
 
         // update table for TRUE
-        if ((isTrue[i][k - 1] && isFalse[k + 1][j]) || (isFalse[i][k - 1] && isTrue[k + 1][j])) {
+        if (isTrue[i][k - 1] && isFalse[k + 1][j]) {
             isTrue[i][j] = true;
+            updateSolution(solution, true, XOR, i, k, j, true, false);
+        }
+        if (isFalse[i][k - 1] && isTrue[k + 1][j]) {
+            isTrue[i][j] = true;
+            updateSolution(solution, true, XOR, i, k, j, false, true);
         }
 
         // update table for FALSE
-        if ((isFalse[i][k - 1] && isFalse[k + 1][j]) || (isTrue[i][k - 1] && isTrue[k + 1][j])) {
+        if (isFalse[i][k - 1] && isFalse[k + 1][j]) {
             isFalse[i][j] = true;
+            updateSolution(solution, false, XOR, i, k, j, false, false);
+        }
+        if (isTrue[i][k - 1] && isTrue[k + 1][j]) {
+            isFalse[i][j] = true;
+            updateSolution(solution, false, XOR, i, k, j, true, true);
         }
     }
 
--- src/test/java/com/max/erickson/dynamic/LongLiveBooleTest.java
+++ src/test/java/com/max/erickson/dynamic/LongLiveBooleTest.java
@@ -8,8 +8,29 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 public class LongLiveBooleTest {
 
     @Test
+    public void findTrueExpressionForExistingSolution() {
+        assertThat(LongLiveBoole.findTrueExpression("T*F^T")).
+                isNotEmpty().
+                hasValue("((T * F) ^ T)");
+
+        assertThat(LongLiveBoole.findTrueExpression("T*T+F^F*F+T^F")).
+                isNotEmpty().
+                hasValue("(((((T * T) + F) ^ (F * F)) + T) ^ F)");
+    }
+
+    @Test
+    public void findTrueExpressionForNotExistingCase() {
+        assertThat(LongLiveBoole.findTrueExpression("F*T^F")).
+                isEmpty();
+    }
+
+    @Test
     public void canBeTrue() {
+        // ((T * F) ^ T) = true
         assertThat(LongLiveBoole.canBeTrue("T*F^T")).isTrue();
+
+        // (((((T * T) + F) ^ (F * F)) + T) ^ F) = true
+        assertThat(LongLiveBoole.canBeTrue("T*T+F^F*F+T^F")).isTrue();
     }
 
     @Test
