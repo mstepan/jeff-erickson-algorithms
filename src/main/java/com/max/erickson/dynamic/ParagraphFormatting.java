895c39777df0e4bc52061e32d1c9668b7065c6a8
Maksym Stepanenko <maksym.stepanenko@opower.com>
2020-05-03 12:34:26 +0300

    Exercise 11. Paragraph formatting with minimum slop. Dynamic programming solution added. No tests yet.

--- src/main/java/com/max/erickson/dynamic/ParagraphFormatting.java
+++ src/main/java/com/max/erickson/dynamic/ParagraphFormatting.java
@@ -16,10 +16,10 @@ public class ParagraphFormatting {
      * space: O(N^2)
      *
      * @param words - all words to be formatted
-     * @param L     - maximum allowed paragraph length
+     * @param paragraphLength     - maximum allowed paragraph length
      * @return lines of paragraph with minimum slop.
      */
-    public static List<String> optimalFormatting(String[] words, int L) {
+    public static List<String> optimalFormatting(String[] words, int paragraphLength) {
 
         final int size = words.length;
 
@@ -36,11 +36,12 @@ public class ParagraphFormatting {
             for (int j = i; j >= 0; --j) {
                 curSize += words[j].length() + 1;
 
-                if (curSize > L) {
+                if (curSize > paragraphLength) {
                     break;
                 }
 
-                int curSlope = slope(curSize, L, j, i, i == size - 1) + (j == 0 ? 0 : opt[j - 1]);
+                boolean isLastLine = i == size - 1;
+                int curSlope = slope(curSize, paragraphLength, j, i, isLastLine) + (j == 0 ? 0 : opt[j - 1]);
 
                 if (curSlope < minSlope) {
                     minSlope = curSlope;
@@ -96,56 +97,73 @@ public class ParagraphFormatting {
      * Specifically, if a line contains words i through j, then the slop of that line is
      * defined to be slope = (L - j + i + totalWordsLength)^3
      */
-    private static int slope(int curSize, int L, int from, int to, boolean lastLine) {
+    private static int slope(int curSize, int paragraphLength, int from, int to, boolean lastLine) {
 
         if (lastLine) {
             return 0;
         }
 
-        int leftSpaces = L - curSize;
+        int leftSpaces = paragraphLength - curSize;
         return leftSpaces * leftSpaces * leftSpaces;
     }
 
     public static void main(String[] args) {
 
+        String[] words = """
+                You and your eight-year-old nephew Elmo decide to play a simple card
+                game. At the beginning of the game, the cards are dealt face up in a long
+                row. Each card is worth a different number of points. After all the cards are
+                dealt, you and Elmo take turns removing either the leftmost or rightmost
+                card from the row, until all the cards are gone. At each turn, you can decide
+                which of the two cards to take. The winner of the game is the player that
+                has collected the most points when the game ends.
+                """.split("\\s+");
 
+        final int paragraphLength = 77;
 
-        String[] words = ("You and your eight-year-old nephew Elmo decide to play a simple card " +
-                "game. At the beginning of the game, the cards are dealt face up in a long " +
-                "row. Each card is worth a different number of points. After all the cards are " +
-                "dealt, you and Elmo take turns removing either the leftmost or rightmost " +
-                "card from the row, until all the cards are gone. At each turn, you can decide " +
-                "which of the two cards to take. The winner of the game is the player that " +
-                "has collected the most points when the game ends.")
-                .split("\\s+");
+        List<String> actualFormatting = optimalFormatting(words, paragraphLength);
 
-
-        int L = 77;
-
-        for (String singleLine : optimalFormatting(words, L)) {
+        for (String singleLine : actualFormatting) {
             System.out.printf("%s ==> %d%n", singleLine, singleLine.length());
         }
 
         System.out.println("===================================");
+        System.out.printf("actual score: %d%n", calculateScore(actualFormatting, paragraphLength));
+        System.out.println("===================================");
 
-        String[] expected = ("You and your eight-year-old nephew Elmo decide to play a simple card\n" +
-                "game. At the beginning of the game, the cards are dealt face up in a long\n" +
-                "row. Each card is worth a different number of points. After all the cards are\n" +
-                "dealt, you and Elmo take turns removing either the leftmost or rightmost\n" +
-                "card from the row, until all the cards are gone. At each turn, you can decide\n" +
-                "which of the two cards to take. The winner of the game is the player that\n" +
-                "has collected the most points when the game ends.").split("\n");
+        String[] expected = """
+                You and your eight-year-old nephew Elmo decide to play a simple card
+                game. At the beginning of the game, the cards are dealt face up in a long
+                row. Each card is worth a different number of points. After all the cards are
+                dealt, you and Elmo take turns removing either the leftmost or rightmost
+                card from the row, until all the cards are gone. At each turn, you can decide
+                which of the two cards to take. The winner of the game is the player that
+                has collected the most points when the game ends.
+                """.split("\n");
 
         for (String singleLine : expected) {
             System.out.printf("%s ==> %d%n", singleLine, singleLine.length());
         }
 
-        System.out.println(cube(3) + cube(3) + cube(2) + cube(4) + cube(6));
-        System.out.println(cube(9) + cube(4) + cube(5) + cube(4));
-
+        System.out.println("===================================");
+        System.out.printf("expected score: %d%n", calculateScore(expected, paragraphLength));
+        System.out.println("===================================");
     }
 
-    private static int cube(int value){
+    private static int calculateScore(List<String> lines, int paragraphLength) {
+        return calculateScore(lines.toArray(new String[]{}), paragraphLength);
+    }
+
+    private static int calculateScore(String[] lines, int paragraphLength) {
+
+        int score = 0;
+        for (int i = 0; i < lines.length - 1; ++i) {
+            score += cube(paragraphLength - lines[i].length());
+        }
+        return score;
+    }
+
+    private static int cube(int value) {
         return value * value * value;
     }
 
